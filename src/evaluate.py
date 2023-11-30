8703a55e0a02157d74ac9502599978d77bcc4cf2
pmcafo <pmcafo@proton.me>
2023-11-30 11:47:28 +0000

    pushing latest changes

--- src/evaluate.py
+++ src/evaluate.py
@@ -44,4 +44,6 @@ for epoch in range(10):  # 10 epochs
         input_ids = tokenizer(document, return_tensors='pt', truncation=True, padding=True, max_length=512)['input_ids']
         labels = tokenizer(summary, return_tensors='pt', truncation=True, padding=True, max_length=150)['input_ids']
 
-  
+        # Compute the loss
+        outputs = model(input_ids=input_ids, labels=labels)
+        l
